1a34dfc5ff37a58d0dc7c2c8c9617a2885c23971
StSerjj <Stepin91@inbox.ru>
2024-09-26 13:13:55 +0300

    add switch in function

--- bases_Installer-v5.ps1
+++ bases_Installer-v5.ps1
@@ -5,12 +5,16 @@ $target_group_2 = 'config_base_2'
 $target_group_3 = 'config_base_3'
 $ibases_path = "C:\Users\$user\AppData\Roaming\1C\1CEStart\ibases.v8i"
 function users_groups {(New-Object System.DirectoryServices.DirectorySearcher("(&(objectCategory=User)(samAccountName=$user))")).FindOne().GetDirectoryEntry().memberOf}
-switch -wildcard (users_groups)
+function ibases_create 
 {
-    "*$target_group_1*" {New-Item -Path "$ibases_path" -Force
-                         Add-Content -Path "$ibases_path" "$work_uh `n$alfazup"}
-    "*$target_group_2*" {New-Item -Path "$ibases_path" -Force
-                         Add-Content -Path "$ibases_path" "$profistaf `n$svoyarabota `n$alfazup"}
-    "*$target_group_3*" {New-Item -Path "$ibases_path" -Force
-                         Add-Content -Path "$ibases_path" "$bp_all"}
+    switch -wildcard (users_groups)
+        {
+        "*$target_group_1*" {New-Item -Path "$ibases_path" -Force
+                             Add-Content -Path "$ibases_path" "$work_uh `n$alfazup"}
+        "*$target_group_2*" {New-Item -Path "$ibases_path" -Force
+                             Add-Content -Path "$ibases_path" "$profistaf `n$svoyarabota `n$alfazup"}
+        "*$target_group_3*" {New-Item -Path "$ibases_path" -Force
+                             Add-Content -Path "$ibases_path" "$bp_all"}
+        }
 }
+ibases_create
