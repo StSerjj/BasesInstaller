c201cf1a2ec92f41b60611f0d08a9b4c0ef3705f
StSerjj <Stepin91@inbox.ru>
2024-09-26 12:44:29 +0300

    add logic in function

--- bases_Installer-v5.ps1
+++ bases_Installer-v5.ps1
@@ -4,8 +4,8 @@ $target_group_1 = 'config_base_1'
 $target_group_2 = 'config_base_2'
 $target_group_3 = 'config_base_3'
 $ibases_path = "C:\Users\$user\AppData\Roaming\1C\1CEStart\ibases.v8i"
-$users_groups = (New-Object System.DirectoryServices.DirectorySearcher("(&(objectCategory=User)(samAccountName=$user))")).FindOne().GetDirectoryEntry().memberOf
-switch -wildcard ($users_groups)
+function users_groups {(New-Object System.DirectoryServices.DirectorySearcher("(&(objectCategory=User)(samAccountName=$user))")).FindOne().GetDirectoryEntry().memberOf}
+switch -wildcard (users_groups)
 {
     "*$target_group_1*" {New-Item -Path "$ibases_path" -Force
                          Add-Content -Path "$ibases_path" "$work_uh `n$alfazup"}
